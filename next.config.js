0b2ae309ed269e503efbc35cd5b58b5365888af0
AgusBarizi <m3gaplazma@gmail.com>
2022-11-10 13:25:50 +0700

    feat: support pwa

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require('next-pwa');
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  reactStrictMode: true,
+});
+
+module.exports = nextConfig;
